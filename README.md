6a507608d4b4cc27d2b4f279bd46a81d70d3a76c
tiger7815 <141488144+tiger7815@users.noreply.github.com>
2023-11-09 16:10:09 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,10 @@
+# txt uploader
+
+# MADE BY MR. Tiger
+
+
+## DEPLOY TO HEROKU
+
+
+[![Deploy to heroku chacha](https://www.herokucdn.com/deploy/button.svg)](https://dashboard.heroku.com/new?template=https://github.com/tiger7815/tiger-txt)
+
---
+++ config.py
@@ -0,0 +1,23 @@
+import os
+
+API_ID = API_ID = 22609670
+
+API_HASH = os.environ.get("API_HASH", "3506d8474ad1f4f5e79b7c52a5c3e88d")
+
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6510228761:AAF0O8Dkol_7gjYzojFccmToaMLHosR36Zg")
+
+PASS_DB = int(os.environ.get("PASS_DB", "7815"))
+
+OWNER = int(os.environ.get("OWNER", 6981453498))
+
+LOG = -1001934203319
+
+try:
+    ADMINS=[]
+    for x in (os.environ.get("ADMINS", "6981453498", "5987970971").split()):
+        ADMINS.append(int(x))
+except ValueError:
+        raise Exception("Your Admins list does not contain valid integers.")
+ADMINS.append(OWNER)
+
+
